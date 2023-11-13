1391832ed1509ad5b1bf865837df36411c8e7f19
Temich sslssll <ramizmatlabov923@gmail.com>
2023-11-13 19:25:50 +0500

    installing react_app

---
+++ frontend/src/components/NoPage.jsx
@@ -0,0 +1,13 @@
+const NoPage = () => {
+    return (
+        <div>
+            <div className="no-page">
+                <h1>
+                    404
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default NoPage;
